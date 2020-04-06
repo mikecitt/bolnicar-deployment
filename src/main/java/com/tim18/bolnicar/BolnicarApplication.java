4e4fd26c50e37e0a49bcf36fdd86866802959d02
Mikec <mikecitt@gmail.com>
2020-04-06 18:02:52 +0200

    Postavljanje inicijalnog rešenja Spring framework-a. Task #1a

---
+++ src/main/java/com/tim18/bolnicar/BolnicarApplication.java
@@ -0,0 +1,13 @@
+package com.tim18.bolnicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolnicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolnicarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim18/bolnicar/BolnicarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim18.bolnicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolnicarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
