6858ba5950f3465f83f2ce5d95b1438c116fc88a
mikecitt <mikecitt@gmail.com>
2020-04-09 12:41:28 +0200

    Napravljena forma za dodavanje lekara i namestena adekvatna ruta. #16b

--- src/main/java/com/tim18/bolnicar/controller/AdminClController.java
+++ src/main/java/com/tim18/bolnicar/controller/AdminClController.java
@@ -1,4 +1,37 @@
 package com.tim18.bolnicar.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/admincl")
 public class AdminClController {
+
+    private static ArrayList<Map<String, String>> doctors =  new ArrayList<>();
+
+    @PostMapping(
+            path="/add",
+            consumes = { MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_JSON_VALUE }
+    )
+
+    public ResponseEntity<HashMap<String, String>> addDoctor(@RequestBody Map<String, String> newDoctor) {
+        HashMap<String, String> response = new HashMap<>();
+
+        if(doctors.contains(newDoctor)) {
+            response.put("message", "false");
+        }
+        else {
+            response.put("message", "true");
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
