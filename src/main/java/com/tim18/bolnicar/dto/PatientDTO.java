071b389d15a8f09b30ab33f20844de81bff49e54
zdugi <zdravko.dugi@gmail.com>
2020-05-06 15:48:41 +0200

    kreiran endpoint i servis za dobavljanje pacijenata

---
+++ src/main/java/com/tim18/bolnicar/dto/PatientDTO.java
@@ -0,0 +1,34 @@
+package com.tim18.bolnicar.dto;
+
+public class PatientDTO {
+    private String jmbg;
+    private String firstName;
+    private String lastName;
+
+    public PatientDTO() {
+    }
+
+    public String getJmbg() {
+        return jmbg;
+    }
+
+    public void setJmbg(String jmbg) {
+        this.jmbg = jmbg;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
