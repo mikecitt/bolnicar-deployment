b2ecc397376253cbd62c969187a6f5a6fe5e0e80
Mikec <mikecitt@gmail.com>
2020-06-12 18:16:27 +0200

    Overrideovan addResourceHandlers(), dodata redirekcija na index.html

--- src/main/java/com/tim18/bolnicar/config/WebConfig.java
+++ src/main/java/com/tim18/bolnicar/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.tim18.bolnicar.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
@@ -14,5 +19,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("https://bolnicar.herokuapp.com/");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException, IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 }
