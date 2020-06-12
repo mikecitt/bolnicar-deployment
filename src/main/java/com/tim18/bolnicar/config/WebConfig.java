d048838707ef718620c19e1a68fbf37fff4bbfc4
zdugi <zdravko.dugi@gmail.com>
2020-06-12 16:45:09 +0200

    izmena

--- src/main/java/com/tim18/bolnicar/config/WebConfig.java
+++ src/main/java/com/tim18/bolnicar/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.tim18.bolnicar.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     // Za svrhe razvoja konfigurisemo dozvolu za CORS kako ne bismo morali @CrossOrigin anotaciju da koristimo nad svakim kontrolerom
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("https://bolnicar.herokuapp.com/");
     }
 }
