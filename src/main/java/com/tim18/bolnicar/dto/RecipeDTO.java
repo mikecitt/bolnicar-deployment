397016dcc13cf6e7c74cbab35a1cf26a668cd9be
roki021 <26367195+roki021@users.noreply.github.com>
2020-06-14 17:50:53 +0200

    Implementirana komponenta i endpoint za overu recepta. #19a #19b

--- src/main/java/com/tim18/bolnicar/dto/RecipeDTO.java
+++ src/main/java/com/tim18/bolnicar/dto/RecipeDTO.java
@@ -9,17 +9,23 @@ import java.util.Set;
 public class RecipeDTO {
     private Integer id;
     private Drug drug;
-    // private Boolean sealed;
+    private Boolean sealed;
+    private String givenBy;
+    private Integer appointmentId;
 
 
-    public RecipeDTO(Integer id, Drug drug) {
+    public RecipeDTO(Integer id, Drug drug, Boolean sealed, String givenBy, Integer appointmentId) {
         this.id = id;
         this.drug = drug;
+        this.sealed = sealed;
+        this.givenBy = givenBy;
+        this.appointmentId = appointmentId;
     }
 
     public RecipeDTO(Recipe recipe) {
         this.id = recipe.getId();
         this.drug = recipe.getDrug();
+        this.sealed = recipe.getSealed();
     }
 
     public RecipeDTO() {
@@ -40,5 +46,29 @@ public class RecipeDTO {
 
     public void setDrug(Drug drug) {
         this.drug = drug;
+    }
+
+    public Boolean getSealed() {
+        return sealed;
+    }
+
+    public void setSealed(Boolean sealed) {
+        this.sealed = sealed;
+    }
+
+    public String getGivenBy() {
+        return givenBy;
+    }
+
+    public void setGivenBy(String givenBy) {
+        this.givenBy = givenBy;
+    }
+
+    public Integer getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(Integer appointmentId) {
+        this.appointmentId = appointmentId;
     }
 }
